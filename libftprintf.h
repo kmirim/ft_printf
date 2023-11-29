3f4d18943d5f63fa767ab1b66cabd7e281bd1be4
Kaluanã Mirim <132582320+kmirim@users.noreply.github.com>
2023-11-29 06:18:51 -0300

    Add files via upload

--- libftprintf.h
+++ libftprintf.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lbarcelo <lbarcelo@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/25 17:58:43 by lbarcelo          #+#    #+#             */
-/*   Updated: 2023/11/26 17:36:13 by lbarcelo         ###   ########.fr       */
+/*   Created: 2023/11/29 05:46:51 by lbarcelo          #+#    #+#             */
+/*   Updated: 2023/11/29 06:06:55 by lbarcelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,20 @@
 
 # include <stdarg.h>
 # include <unistd.h>
-# include "../libft/libft.h"
+# include <stdlib.h>
 
 int		ft_putchar(char c);
 int		ft_putnbr(int n);
 int		ft_putstr(char *s);
-int		ft_numlen(int n, int base);
-char	*ft_putnbr_base(int n, int base, char *digits);
+char	*ft_putnbr_base(long long n, int base, char *digits);
 int		ft_printf(const char *format, ...);
+int		ft_putnbr_u(unsigned int n);
+int		ft_puthex(unsigned long long n, const char fmt);
+int		ft_printf_pointer(unsigned long long n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_itoa(int n);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
